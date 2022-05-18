7e6907ecc9aa507cb73241758439aaa812a6d1cc
Fahrul Thariq Fadillah <fahrul.thoriq@gmail.com>
2022-05-18 18:41:17 +0700

    fix bullet fire & fix menu UI

---
+++ Assets/Scenes/Script/collisionScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionScript : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject explosion = Instantiate(Resources.Load("energyBlast", typeof(GameObject))) as GameObject;
+        explosion.transform.position = transform.position;
+        Destroy(col.gameObject);
+        Destroy(explosion, 2);
+
+
+        if (GameObject.FindGameObjectsWithTag("Player").Length == 0)
+        {
+
+            GameObject enemy = Instantiate(Resources.Load("enemy", typeof(GameObject))) as GameObject;
+            GameObject enemy1 = Instantiate(Resources.Load("enemy1", typeof(GameObject))) as GameObject;
+            GameObject enemy2 = Instantiate(Resources.Load("enemy2", typeof(GameObject))) as GameObject;
+
+        }
+
+        Destroy(gameObject);
+
+
+    }
+
+}
---
+++ Assets/Scenes/Script/mainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void Startlevel1()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void SettingGame()
+    {
+        SceneManager.LoadScene("SettingScene");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        SceneManager.LoadScene("Quit");
+    }
+}
